7bb269e50779470bb05d6416745caa6056f1d9af
Mecit Semerci <mecit.semerci@gmail.com>
2019-01-23 00:40:56 +0300

    Add user properties

--- Src/PonyUrl.Infrastructure.MongoDb.Identity/Models/ApplicationUser.cs
+++ Src/PonyUrl.Infrastructure.MongoDb.Identity/Models/ApplicationUser.cs
@@ -1,11 +1,17 @@
 ﻿using AspNetCore.Identity.Mongo.Model;
+using PonyUrl.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PonyUrl.Infrastructure.MongoDb.Identity.Models
 {
-    public class ApplicationUser : MongoUser
+    public class ApplicationUser : MongoUser, IUser
     {
+        public string UserId => Id;
+
+        public string DisplayName { get; set; }
+
+       
     }
 }
