14911c594deaa442869cc821d32345de53ee9524
Mecit Semerci <mecit.semerci@gmail.com>
2019-01-23 00:54:44 +0300

    Fixed referenced problems

---
+++ Src/PonyUrl.Infrastructure.MongoDb.Identity/Authorization/JwtTokenOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Threading.Tasks;
+
+namespace PonyUrl.Infrastructure.MongoDb.Identity.Authorization
+{
+    public class JwtTokenOptions
+    {
+        public string Issuer { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Audience { get; set; }
+
+        public DateTime NotBefore { get; set; } = DateTime.UtcNow;
+
+        public DateTime IssueAt { get; set; } = DateTime.UtcNow;
+
+        public TimeSpan ValidFor { get; set; } = TimeSpan.FromHours(5);
+
+        public DateTime Expiration => IssueAt.Add(ValidFor);
+
+        public Func<Task<string>> JtiGenerator => () => Task.FromResult(Guid.NewGuid().ToString());
+
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+
+}
