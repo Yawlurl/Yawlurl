e93b5e0fcf27757464bbe445301e780bd7fc51c0
Mecit Semerci <mecit.semerci@gmail.com>
2019-10-26 02:40:17 +0300

    Web Api changes

---
+++ Src/PonyUrl.Web.Api/Filters/SwaggerHeaderFilter.cs
@@ -0,0 +1,34 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PonyUrl.Web.Api.Filters
+{
+    public class SwaggerHeaderFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<IParameter>();
+
+            operation.Parameters.Add(new NonBodyParameter
+            {
+                Name = "consumerCode",
+                In = "header",
+                Type = "string",
+                Required = false
+            });
+
+            operation.Parameters.Add(new NonBodyParameter
+            {
+                Name = "traceId",
+                In = "header",
+                Type = "string",
+                Required = false
+            });
+        }
+    }
+}
