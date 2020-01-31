69045ba640cca867849457a1167ff247eb9cb802
Mecit Semerci <mecit.semerci@gmail.com>
2020-01-31 11:21:55 +0300

    Added router model for anonymous access

---
+++ Src/YawlUrl.Web.Router/Models/GenerateUrlModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YawlUrl.Web.Router.Models
+{
+    public class GenerateUrlModel
+    {
+        [Required]
+        public string LongUrl { get; set; }
+    }
+}
